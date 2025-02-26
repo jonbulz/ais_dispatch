13b855ec331c15838f71f5feb9f68fe8cea20941
Jonas Buchholz <jonas.buchholz@altaia.net>
2025-02-26 13:07:41 +0100

    request authentication and sent data tracking

--- dispatch/dispatch.py
+++ dispatch/dispatch.py
@@ -1,29 +1,124 @@
-import psycopg2
 import requests
 import os
+import io
+import csv
+import time
+import json
+from urllib.parse import urljoin
+from utils.db import get_config_value, fetch_data, update_data_sent_size
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-REMOTE_HOST = "http://example.com/endpoint"
 
+class Dispatcher:
+    def __init__(self):
+        self.remote_host = os.getenv("REMOTE_HOST")
+        self.user = os.getenv("REMOTE_USER")
+        self.secret = os.getenv("REMOTE_SECRET")
+        self.token = None
 
-def dispatch():
-    conn = psycopg2.connect(DATABASE_URL)
-    cursor = conn.cursor()
+    @staticmethod
+    def can_send_data(payload):
+        data_max = get_config_value("data_max")
+        data_sent = get_config_value("data_sent")
+        return len(payload) <= data_max - data_sent
 
-    cursor.execute("SELECT id, payload FROM data ORDER BY received_at ASC")
-    rows = cursor.fetchall()
+    @staticmethod
+    def is_active():
+        return get_config_value("active")
 
-    for row in rows:
-        data_id, payload = row
-        response = requests.post(REMOTE_HOST, json={"data": payload})
+    @staticmethod
+    def _ais_to_csv(data):
+        out = io.StringIO()
 
-        if response.status_code == 200:
-            cursor.execute("DELETE FROM data WHERE id = %s", (data_id,))
-            conn.commit()
+        # todo define cols in a central place?
+        cols = ["timestamp", "identifier", "lat", "lon", "cog", "sog"]
+        required = ["timestamp", "identifier", "lat", "lon"]
+        writer = csv.DictWriter(out, fieldnames=cols)
+        writer.writeheader()
+        for identifier, vals in data.items():
+            # DictWriter is strict about matching the columns
+            # so we need to filter our values
+            vals = {k: v for k, v in vals.items() if k in cols}
+            incomplete_data = {key for key in required if not vals.get(key)}
+            if incomplete_data:
+                continue
+            row = {"identifier": identifier, **vals}
+            writer.writerow(row)
+        csv_data = out.getvalue()
+        out.close()
+        return csv_data
 
-    cursor.close()
-    conn.close()
+    def _get_new_token(self):
+        url = urljoin(self.remote_host, "token")
+        headers = {"Content-Type": "application/json"}
+        credentials = {"username": self.user, "password": self.secret}
+        data = json.dumps(credentials)
+        auth_response = self._send_request("POST", url, headers=headers, data=data)
+        auth_response.raise_for_status()
+        token = auth_response.json().get("access")
+        return token
+
+    def _update_token(self, token=None):
+        if not token:
+            token = self._get_new_token()
+        self.token = token
+
+    @staticmethod
+    def _send_request(method, url, **kwargs):
+        """
+        Wrapper to track sent data size
+        :param method: HTTP method
+        :param url: The request url
+        :return: Response object
+        """
+        headers_size = sum(len(k) + len(v) for k, v in kwargs.get("headers", {}).items())
+        body_size = len(kwargs.get("data", b"")) if "data" in kwargs else 0
+        json_size = len(str(kwargs.get("json", ""))) if "json" in kwargs else 0
+
+        total_sent_size = headers_size + body_size + json_size
+
+        response = requests.request(method, url, **kwargs)
+
+        update_data_sent_size(total_sent_size)
+
+        return response
+
+    def _authenticate_request(self, method, url, **kwargs):
+        """
+        Wrapper to handle authentication
+        :param method: HTTP method
+        :param url: The request url
+        :return: Response object
+        """
+        if not self.token:
+            self._update_token()
+        headers = kwargs.pop("headers", {})
+        headers["Authorization"] = f"Bearer {self.token}"
+
+        response = self._send_request(method, url, headers=headers, **kwargs)
+        if response.status_code == 401:
+            self._update_token()
+            headers["Authorization"] = f"Bearer {self.token}"
+            response = self._send_request(method, url, headers=headers, **kwargs)
+
+        response.raise_for_status()
+        return response
+
+    def _dispatch(self, payload):
+        url = urljoin(self.remote_host, "ais/add")
+        return self._authenticate_request("POST", url, data=payload)
+
+    def start_dispatch(self):
+        while True:
+            if not self.is_active():
+                break
+            data = fetch_data()
+            payload = self._ais_to_csv(data)
+            if not self.can_send_data(payload):
+                break
+            self._dispatch(payload)
+            time.sleep(get_config_value("interval"))
 
 
 if __name__ == "__main__":
-    dispatch()
+    dispatcher = Dispatcher()
+    dispatcher.start_dispatch()
--- utils/db.py
+++ utils/db.py
@@ -15,3 +15,36 @@ def insert_data(pk, payload):
     conn.commit()
     cursor.close()
     conn.close()
+
+
+def fetch_data():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, payload FROM data ORDER BY received_at ASC")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return rows
+
+
+def get_config_value(key):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT value FROM config WHERE key = %s;", (key,))
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return result[0] if result else None
+
+
+def update_data_sent_size(size):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE config
+        SET value = (value::BIGINT + %s)::TEXT
+        WHERE key = 'data_sent';
+    """, (size,))
+    conn.commit()
+    cursor.close()
+    conn.close()
